74697cfbaa93c9b2168cd14b42b1ab0488ed0d49
Армен Мкртчян <MkrtchyanAS@outlook.com>
2018-07-20 11:30:33 +0500

    tutorial completed

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -62,4 +62,32 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
+  /** POST: add a new hero to the server */
+  addHero (newHero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, newHero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  searchHeroes (term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap (_ => this.log(`found heroes matching ${term}`)),
+      catchError(this.handleError<Hero[]>('search heroes', []))
+    );
+  }
 }
